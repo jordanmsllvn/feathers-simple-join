1303f754d261fdc1104fb14016cbb6d41f429929
Jordan Sullivan <jordanmsllvn@gmail.com>
2021-04-19 19:56:54 -0600

    fixing

--- src/simpleJoin.ts
+++ src/simpleJoin.ts
@@ -116,7 +116,7 @@ export default async function simpleJoin(
       // Attach any extra fields existing in the join records themselves
       if(options.through!.attach && options.through!.attach.length){
         r[options.with.as] = r[options.with.as].map((rm: any) => {
-          const throughRecord = joins.find((j: any) => { return j[options.through!.remote] === rm[options.with.remote]; })
+          const throughRecord = joins.find((j: any) => { return j[options.through!.remote] === rm[options.with.remote] && j[options.through!.local] === r[options.with.local]; })
           for(let attachment of options.through!.attach!){
             rm[attachment] = throughRecord[attachment];
           }
